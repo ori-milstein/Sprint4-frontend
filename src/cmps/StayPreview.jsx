45d0b6977e27cb07ea614a3db74d24097a4bd97b
ori-milstein <mydoal@keemail.me>
2025-01-06 18:28:34 +0200

    Change preivew price to dollars

--- src/cmps/StayPreview.jsx
+++ src/cmps/StayPreview.jsx
@@ -6,7 +6,7 @@ export function StayPreview({ stay }) {
             <Link to={`/stay/${stay._id}`}>{stay.loc.city}, {stay.loc.country}</Link>
         </header>
 
-        <p><span>₪{stay.price}</span> night</p>
+        <p><span>${stay.price}</span> night</p>
         {stay.owner && <p>Owner: <span>{stay.owner.fullname}</span></p>}
 
     </article>
