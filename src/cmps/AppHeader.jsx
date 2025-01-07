2d6dc05f60a402cd63d01a8833c05e3b50ba8aa6
tal.peretz <tal.peretz@tufin.com>
2025-01-07 16:40:57 +0200

    Header styling

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -5,10 +5,13 @@ import { showErrorMsg, showSuccessMsg } from '../services/event-bus.service'
 import { logout } from '../store/actions/user.actions'
 import Logo from './Logo'
 import { HeaderFilter } from './HeaderFilter'
+import { useState } from 'react'
+
 
 export function AppHeader() {
 	const user = useSelector(storeState => storeState.userModule.user)
 	const navigate = useNavigate()
+	const [isExpanded, setIsExpanded] = useState(false)
 
 	async function onLogout() {
 		try {
@@ -21,26 +24,26 @@ export function AppHeader() {
 	}
 
 	return (
-			<header className="app-header full">
-				<nav>
-					<NavLink to="/" className="logo">
-						<Logo />
-						<h1>airbnb</h1>
-					</NavLink>
-					<HeaderFilter></HeaderFilter>
-					{user?.isAdmin && <NavLink to="/admin">Admin</NavLink>}
-					{!user && <NavLink to="login" className="login-link">Login</NavLink>}
-					{user && (
-						<div className="user-info">
-							<Link to={`user/${user._id}`}>
-								{/* {user.imgUrl && <img src={user.imgUrl} />} */}
-								{user.fullname}
-							</Link>
-							<button onClick={onLogout}>logout</button>
-						</div>
-					)}
-				</nav>
-			</header>
+		<header className="app-header full">
+			 <nav className={isExpanded ? 'expand' : ''}>
+				<NavLink to="/" className="logo">
+					<Logo />
+					<h1>airbnb</h1>
+				</NavLink>
+				<HeaderFilter isExpanded={isExpanded} setIsExpanded={setIsExpanded} />
+				{user?.isAdmin && <NavLink to="/admin">Admin</NavLink>}
+				{!user && <NavLink to="login" className="login-link">Login</NavLink>}
+				{user && (
+					<div className="user-info">
+						<Link to={`user/${user._id}`}>
+							{/* {user.imgUrl && <img src={user.imgUrl} />} */}
+							{user.fullname}
+						</Link>
+						<button onClick={onLogout}>logout</button>
+					</div>
+				)}
+			</nav>
+		</header>
 
 
 	)
--- src/cmps/HeaderFilter.jsx
+++ src/cmps/HeaderFilter.jsx
@@ -1,35 +1,66 @@
-export function HeaderFilter() {
+export function HeaderFilter({isExpanded, setIsExpanded}) {
+    console.log('is EXPANEDED:', isExpanded)
     return (
-        <form className="filter-container">
-            <div className="filter-action-container">
-                <label htmlFor="filter-where" className="filter-label">Where</label>
-                <input id="filter-where" className="filter-action filter-where" placeholder="Search destinations"></input>
-            </div>
-            <div className="filter-action-container">
-                <label htmlFor="filter-checkin" className="filter-label">Check in</label>
-                <input id="filter-checkin" className="filter-action filter-checkin" placeholder="Add dates"></input>
-            </div>
-            <div className="filter-action-container">
-                <label htmlFor="filter-checkout" className="filter-label">Check out</label>
-                <input id="filter-checkout" className="filter-action filter-checkout" placeholder="Add dates"></input>
-            </div>
-            <div className="filter-action-container who">
-                <label htmlFor="filter-who" className="filter-label">Who</label>
-                <input id="filter-who" className="filter-action filter-who" placeholder="Add guests"></input>
-            </div>
-            <button className="filter-search"><svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 32 32"
-                width="13"
-                height="13"
-                stroke="white"
-                stroke-width="4"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-            >
-                <path fill="none" d="M13 24a11 11 0 1 0 0-22 11 11 0 0 0 0 22zm8-3 9 9"></path>
-            </svg>
-            </button>
-        </form>
-    );
+        <>
+            {isExpanded && (
+                <form className="filter-container expanded">
+                    <div className="filter-action-container">
+                        <label htmlFor="filter-where" className="filter-label">Where</label>
+                        <input id="filter-where" className="filter-action filter-where" placeholder="Search destinations"></input>
+                    </div>
+                    <div className="filter-action-container">
+                        <label htmlFor="filter-checkin" className="filter-label">Check in</label>
+                        <input id="filter-checkin" className="filter-action filter-checkin" placeholder="Add dates"></input>
+                    </div>
+                    <div className="filter-action-container">
+                        <label htmlFor="filter-checkout" className="filter-label">Check out</label>
+                        <input id="filter-checkout" className="filter-action filter-checkout" placeholder="Add dates"></input>
+                    </div>
+                    <div className="filter-action-container who">
+                        <label htmlFor="filter-who" className="filter-label">Who</label>
+                        <input id="filter-who" className="filter-action filter-who" placeholder="Add guests"></input>
+                    </div>
+                    <button className="filter-search long-btn"><svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 32 32"
+                        width="13"
+                        height="13"
+                        stroke="white"
+                        stroke-width="4"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                    >
+                        <path fill="none" d="M13 24a11 11 0 1 0 0-22 11 11 0 0 0 0 22zm8-3 9 9"></path>
+                    </svg>
+                    </button>
+                </form>
+            )}
+            {!isExpanded && (
+                <form className="filter-container not-expanded" onClick={()=> setIsExpanded(true)}>
+                    <div className="filter-action-container short anywhere">
+                        <label className="filter-label">Anywhere</label>
+                    </div>
+                    <div className="filter-action-container short anyweek">
+                        <label className="filter-label">Any week</label>
+                    </div>
+                    <div className="filter-action-container short who">
+                        <label className="filter-label add-guests">Add guests</label>
+                    </div>
+                    <button className="filter-search short-btn"><svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 32 32"
+                        width="13"
+                        height="13"
+                        stroke="white"
+                        stroke-width="4"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                    >
+                        <path fill="none" d="M13 24a11 11 0 1 0 0-22 11 11 0 0 0 0 22zm8-3 9 9"></path>
+                    </svg>
+                    </button>
+                </form>
+            )}
+        </>
+    )
 }
