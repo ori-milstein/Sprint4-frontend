9d7861032ee2102701ccf1135e8b1246bd62424b
Andrey Chetvergov <andrey32102@gmail.com>
2025-01-16 17:34:02 +0200

    Make modal in photos page when clicking any img

--- src/pages/PhotosPage.jsx
+++ src/pages/PhotosPage.jsx
@@ -1,16 +1,42 @@
 import { useLocation, useNavigate } from "react-router-dom"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 export function PhotosPage() {
     const location = useLocation()
     const navigate = useNavigate()
     const { imgUrls } = location.state || { imgUrls: [] }
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [currentImageIndex, setCurrentImageIndex] = useState(null);
 
     useEffect(() => {
         if (!imgUrls.length) {
             navigate(-1)
         }
     }, [imgUrls, navigate])
+
+    const openModal = (index) => {
+        setIsModalOpen(true)
+        setCurrentImageIndex(index)
+        document.body.style.overflow = "hidden"
+    }
+
+    const closeModal = () => {
+        setIsModalOpen(false);
+        setCurrentImageIndex(null)
+        document.body.style.overflow = "auto"
+    }
+
+    const navigatePrev = () => {
+        setCurrentImageIndex((prevIndex) =>
+            prevIndex === 0 ? imgUrls.length - 1 : prevIndex - 1
+        )
+    }
+
+    const navigateNext = () => {
+        setCurrentImageIndex((prevIndex) =>
+            prevIndex === imgUrls.length - 1 ? 0 : prevIndex + 1
+        )
+    }
 
     return (
         <div className="photo-gallery-page">
@@ -21,7 +47,6 @@ export function PhotosPage() {
                     alt="Back"
                 />
             </button>
-            {/* <h1 className="photo-gallery-title"></h1> */}
             <div className="photo-gallery">
                 {imgUrls.map((url, index) => (
                     <img
@@ -29,9 +54,29 @@ export function PhotosPage() {
                         src={url}
                         alt={`Photo ${index + 1}`}
                         className={`photo-item div${(index % 5) + 1}`}
+                        onClick={() => openModal(index)}
                     />
                 ))}
             </div>
-        </div>
+            {isModalOpen && (
+                <div className="modal">
+                    <button className="close-btn" onClick={closeModal}>
+                        <span>X</span> Close
+                    </button>
+                    <div className="modal-content">
+                        <img
+                            src={imgUrls[currentImageIndex]}
+                            alt={`Photo ${currentImageIndex + 1}`}
+                        />
+                    </div>
+                    <button className="navigation prev" onClick={navigatePrev}>
+                        &#8249; {/* Left arrow */}
+                    </button>
+                    <button className="navigation next" onClick={navigateNext}>
+                        &#8250; {/* Right arrow */}
+                    </button>
+                </div>
+            )}
+        </div >
     )
 }
