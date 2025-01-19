cb9203760c83d08186e6841670c0c828f96baea3
tal.peretz <tal.peretz@tufin.com>
2025-01-19 17:27:52 +0200

    adding header to stay details

--- src/pages/StayDetails.jsx
+++ src/pages/StayDetails.jsx
@@ -69,124 +69,126 @@ export function StayDetails() {
   }
 
   return (
-    <div className="stay-container">
-      <section className="stay-details">
-        {appModal &&
-          <AppModal isModalActive={isModalActive} setIsModalActive={setIsModalActive} modalType={appModal} stay={stay} />}
+    <><AppHeader isHomepage={false}></AppHeader>
+      <div className="stay-container">
+        <section className="stay-details">
+          {appModal &&
+            <AppModal isModalActive={isModalActive} setIsModalActive={setIsModalActive} modalType={appModal} stay={stay} />}
 
-        <h1 className="stay-details-header">{stay.name}</h1>
+          <h1 className="stay-details-header">{stay.name}</h1>
 
-        <div className="stay-images">
-          {/* Main image */}
-          <img
-            className="main-image"
-            src={stay.imgUrls[0]}
-            alt="Main Stay Image"
-            onClick={handleImgShowAllPhotos}
-          />
-          {/* Side images */}
-          <img
-            className="side-top"
-            src={stay.imgUrls[1]}
-            alt="Side Top Image"
-            onClick={handleImgShowAllPhotos}
-          />
-          <img
-            className="side-middle"
-            src={stay.imgUrls[2]}
-            alt="Side Middle Image"
-            onClick={handleImgShowAllPhotos}
-          />
-          <img
-            className="side-bottom-image"
-            src={stay.imgUrls[3]}
-            alt="Side Bottom Image"
-            onClick={handleImgShowAllPhotos}
-          />
-          <div className="side-extra-container">
+          <div className="stay-images">
+            {/* Main image */}
             <img
-              className="side-extra"
-              src={stay.imgUrls[4]}
-              alt="Side Extra Image"
+              className="main-image"
+              src={stay.imgUrls[0]}
+              alt="Main Stay Image"
               onClick={handleImgShowAllPhotos}
             />
-            <button className={`show-all-btn ${isClicked ? 'clicked' : ''}`}
-              onClick={handleShowAllPhotos}
-            >
+            {/* Side images */}
+            <img
+              className="side-top"
+              src={stay.imgUrls[1]}
+              alt="Side Top Image"
+              onClick={handleImgShowAllPhotos}
+            />
+            <img
+              className="side-middle"
+              src={stay.imgUrls[2]}
+              alt="Side Middle Image"
+              onClick={handleImgShowAllPhotos}
+            />
+            <img
+              className="side-bottom-image"
+              src={stay.imgUrls[3]}
+              alt="Side Bottom Image"
+              onClick={handleImgShowAllPhotos}
+            />
+            <div className="side-extra-container">
               <img
-                src="/src/assets/icons/apps_24dp_000000_FILL0_wght400_GRAD0_opsz24.png"
-                className="icon"
-                alt="Show All Photos"
+                className="side-extra"
+                src={stay.imgUrls[4]}
+                alt="Side Extra Image"
+                onClick={handleImgShowAllPhotos}
               />
-              Show all photos
-            </button>
+              <button className={`show-all-btn ${isClicked ? 'clicked' : ''}`}
+                onClick={handleShowAllPhotos}
+              >
+                <img
+                  src="/src/assets/icons/apps_24dp_000000_FILL0_wght400_GRAD0_opsz24.png"
+                  className="icon"
+                  alt="Show All Photos"
+                />
+                Show all photos
+              </button>
+            </div>
           </div>
-        </div>
-        <div className="stay-details-info">
-          <section className='details-content'>
+          <div className="stay-details-info">
+            <section className='details-content'>
 
-            <section className='subtitles'>
-              <h2 className='subtitle'>
-                {stay.roomType} in {stay.loc.city}, {stay.loc.country}
-              </h2>
-              <h3 className='regular-text'>
-                {stay.capacity} guests  · {stay.equipment.bedroomNum} bedrooms  ·  {stay.equipment.bedsNum} beds  ·  {stay.equipment.bathNum} baths
-              </h3>
-              <h4 className='bold-text reviews-summary'>
-                <img src='../../src/assets/assets/icons/general icons/asset 158.svg' />
-                <span>{stay.reviews.length ? '5.0' : 'New'} · </span>
-                {stay.reviews.length > 0 &&
-                  <a className='nostyle underline' onClick={() => { handleShowMore(SET_APP_MODAL_REVIEWS) }}>{stay.reviews.length} reviews</a>}
-              </h4>
-              <hr className="divider" />
+              <section className='subtitles'>
+                <h2 className='subtitle'>
+                  {stay.roomType} in {stay.loc.city}, {stay.loc.country}
+                </h2>
+                <h3 className='regular-text'>
+                  {stay.capacity} guests  · {stay.equipment.bedroomNum} bedrooms  ·  {stay.equipment.bedsNum} beds  ·  {stay.equipment.bathNum} baths
+                </h3>
+                <h4 className='bold-text reviews-summary'>
+                  <img src='../../src/assets/assets/icons/general icons/asset 158.svg' />
+                  <span>{stay.reviews.length ? '5.0' : 'New'} · </span>
+                  {stay.reviews.length > 0 &&
+                    <a className='nostyle underline' onClick={() => { handleShowMore(SET_APP_MODAL_REVIEWS) }}>{stay.reviews.length} reviews</a>}
+                </h4>
+                <hr className="divider" />
+              </section>
+
+              <div className="stay-host">
+
+                <div className="host-picture">
+                  <img
+                    src={stay.host.imgUrl || stay.host.thumbnailUrl}
+                    alt={`Picture of ${stay.host.fullname}`}
+                  />
+                </div>
+                <div className="host-details">
+                  <p>
+                    <strong>Hosted by:</strong> <span>{stay.host.fullname}</span>
+                  </p>
+                  <p>
+                    <strong>Location:</strong> <span>{stay.host.location}</span>
+                  </p>
+                  <p>
+                    <strong>Response Time:</strong> <span>{stay.host.responseTime}</span>
+                  </p>
+                  <hr className="divider" />
+                  {stay.host.isSuperhost && (
+                    <p className="superhost">
+                      <span>🌟 Superhost</span>
+                    </p>
+                  )}
+                  <p>
+                    <strong>About the Host:</strong> <span>{stay.host.description}</span>
+                  </p>
+                </div>
+                <button onClick={onAddStayMsg}>Add Stay Message</button>
+              </div>
+
+              <div className="stay-summary">
+                <p>{stay.summary}</p>
+              </div>
+              {stay.amenities &&
+                <Amenities stay={stay} />}
             </section>
 
-            <div className="stay-host">
-
-              <div className="host-picture">
-                <img
-                  src={stay.host.imgUrl || stay.host.thumbnailUrl}
-                  alt={`Picture of ${stay.host.fullname}`}
-                />
-              </div>
-              <div className="host-details">
-                <p>
-                  <strong>Hosted by:</strong> <span>{stay.host.fullname}</span>
-                </p>
-                <p>
-                  <strong>Location:</strong> <span>{stay.host.location}</span>
-                </p>
-                <p>
-                  <strong>Response Time:</strong> <span>{stay.host.responseTime}</span>
-                </p>
-                <hr className="divider" />
-                {stay.host.isSuperhost && (
-                  <p className="superhost">
-                    <span>🌟 Superhost</span>
-                  </p>
-                )}
-                <p>
-                  <strong>About the Host:</strong> <span>{stay.host.description}</span>
-                </p>
-              </div>
-              <button onClick={onAddStayMsg}>Add Stay Message</button>
+            <div className="stay-reserve-container">
+              <Reserve />
             </div>
-
-            <div className="stay-summary">
-              <p>{stay.summary}</p>
-            </div>
-            {stay.amenities &&
-              <Amenities stay={stay} />}
-          </section>
-
-          <div className="stay-reserve-container">
-            <Reserve />
           </div>
-        </div>
-        <LocationDetails stay={stay} />
-        <ReviewSection stay={stay} handleShowMore={handleShowMore} isModalActive={isModalActive} setReviewIdxToScroll={setReviewIdxToScroll} />
-      </section>
-    </div>
+          <LocationDetails stay={stay} />
+          <ReviewSection stay={stay} handleShowMore={handleShowMore} isModalActive={isModalActive} setReviewIdxToScroll={setReviewIdxToScroll} />
+        </section>
+      </div>
+    </>
   )
 
 }
