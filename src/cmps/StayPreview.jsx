782250aac1b629d46ab90f2701a56ab842513bc2
Andrey Chetvergov <andrey32102@gmail.com>
2025-01-26 21:07:54 +0200

    make the chevron left and chevron right in carousel controls

--- src/cmps/StayPreview.jsx
+++ src/cmps/StayPreview.jsx
@@ -90,14 +90,14 @@ export function StayPreview({ stay }) {
                                     onClick={handlePrev}
                                     disabled={currentIndex === 0}
                                 >
-                                    <i class="fa-solid fa-arrow-left"></i>
+                                    <i class="fa-solid fa-chevron-left"></i>
                                 </button>
                                 <button
                                     className="next-btn"
                                     onClick={handleNext}
                                     disabled={currentIndex === stay.imgUrls.length - 1}
                                 >
-                                    <i class="fa-solid fa-arrow-right"></i>
+                                    <i class="fa-solid fa-chevron-right"></i>
                                 </button>
                             </div>
                             <div className="carousel-indicators">
